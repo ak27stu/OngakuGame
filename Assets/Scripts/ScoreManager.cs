66c55b13657a7a7964aeb1f65ba88a662a3a7be4
komoririo <75403807+komoririo@users.noreply.github.com>
2021-11-13 11:27:33 +0900

    A6

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -5,10 +5,7 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour {
 
-    public GameObject score_object1 = null; // Textオブジェクト
-    public GameObject score_object2 = null; // Textオブジェクト
-    public GameObject score_object3 = null; // Textオブジェクト
-    public GameObject score_object4 = null; // Textオブジェクト
+    public GameObject score_object = null; // Textオブジェクト
     public int score_num = 0; // スコア変数
 
       // 初期化
@@ -18,28 +15,11 @@ public class ScoreManager : MonoBehaviour {
       // 更新
       void Update () {
         // オブジェクトからTextコンポーネントを取得
-        Text score_text1 = score_object1.GetComponent<Text> ();
+        Text score_text = score_object.GetComponent<Text>();
         // テキストの表示を入れ替える
-        score_text1.text = "Score:" + Judgement.Update();
+        score_text.text = "Score:" + score_num;
 
-        /*
-        if(Judgement.distance<3){
-
-        }
-
-        Text score_text2 = score_object2.GetComponent<Text> ();
-        // テキストの表示を入れ替える
-        score_text2.text = "Score:" + Judgement.AddScore();
-
-        Text score_text3 = score_object3.GetComponent<Text> ();
-        // テキストの表示を入れ替える
-        score_text3.text = "Score:" + Judgement.AddScore();
-
-        Text score_text4 = score_object4.GetComponent<Text> ();
-        // テキストの表示を入れ替える
-        score_text4.text = "Score:" + Judgement.AddScore();
-
-        score_num += 1; // とりあえず1加算し続けてみる
+        /*score_num += 1; // とりあえず1加算し続けてみる
 
         if(score_num==100){
           return;
